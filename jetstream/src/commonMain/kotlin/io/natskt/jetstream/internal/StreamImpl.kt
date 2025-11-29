c955257c692f9b5fe1b9e62364f0288eb5d85845
Nicholas Hassan <nick@hassan.host>
2025-11-29 18:39:49 +1030

    feat(jetstream): add stream management ops

--- jetstream/src/commonMain/kotlin/io/natskt/jetstream/internal/StreamImpl.kt
+++ jetstream/src/commonMain/kotlin/io/natskt/jetstream/internal/StreamImpl.kt
@@ -3,7 +3,10 @@ package io.natskt.jetstream.internal
 import io.natskt.api.ProtocolException
 import io.natskt.api.internal.InternalNatsApi
 import io.natskt.internal.throwOnInvalidToken
+import io.natskt.jetstream.api.ConsumerInfo
 import io.natskt.jetstream.api.JetStreamClient
+import io.natskt.jetstream.api.MessageGetRequest
+import io.natskt.jetstream.api.StoredMessage
 import io.natskt.jetstream.api.StreamInfo
 import io.natskt.jetstream.api.consumer.ConsumerConfigurationBuilder
 import io.natskt.jetstream.api.consumer.PullConsumer
@@ -24,6 +27,8 @@ internal class StreamImpl(
 
 	@OptIn(InternalNatsApi::class)
 	override val info = MutableStateFlow(initialInfo)
+	val supportsDirect: Boolean?
+		get() = info.value?.config?.allowDirect
 
 	override suspend fun updateStreamInfo(): Result<StreamInfo> {
 		val new = js.getStreamInfo(name)
@@ -59,4 +64,42 @@ internal class StreamImpl(
 				)
 			}.getOrThrow()
 	}
+
+	override suspend fun updateConsumer(configure: ConsumerConfigurationBuilder.() -> Unit): ConsumerInfo = js.manager.updateConsumer(this.name, configure)
+
+	override suspend fun getConsumerInfo(name: String): ConsumerInfo = js.manager.getConsumerInfo(this.name, name)
+
+	override suspend fun deleteConsumer(name: String): Boolean = js.manager.deleteConsumer(this.name, name)
+
+	override suspend fun getConsumerNames(): List<String> = js.manager.getConsumerNames(name)
+
+	override suspend fun getConsumers(): List<ConsumerInfo> = js.manager.getConsumers(name)
+
+	override suspend fun getMessage(sequence: ULong): StoredMessage {
+		supportsDirect ?: updateStreamInfo()
+		return js.manager.getMessage(name, sequence, supportsDirect!!)
+	}
+
+	override suspend fun getMessage(request: MessageGetRequest): StoredMessage {
+		supportsDirect ?: updateStreamInfo()
+		return js.manager.getMessage(name, request, supportsDirect!!)
+	}
+
+	override suspend fun getLastMessage(subject: String): StoredMessage {
+		supportsDirect ?: updateStreamInfo()
+		return js.manager.getLastMessage(name, subject, supportsDirect!!)
+	}
+
+	override suspend fun getNextMessage(
+		sequence: ULong,
+		subject: String,
+	): StoredMessage {
+		supportsDirect ?: updateStreamInfo()
+		return js.manager.getNextMessage(name, sequence, subject, supportsDirect!!)
+	}
+
+	override suspend fun deleteMessage(
+		sequence: ULong,
+		erase: Boolean,
+	): Boolean = js.manager.deleteMessage(name, sequence, erase)
 }
