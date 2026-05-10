432106406537d860eb6809629e277f63e12d7b27
Nicholas Hassan <nick@hassan.host>
2026-05-10 20:27:40 +0930

    docs(jetstream): update kdocs

--- jetstream/src/commonMain/kotlin/io/natskt/jetstream/api/kv/KeyValueManager.kt
+++ jetstream/src/commonMain/kotlin/io/natskt/jetstream/api/kv/KeyValueManager.kt
@@ -14,8 +14,10 @@ public interface KeyValueManager {
 	public suspend fun create(configure: KeyValueConfigurationBuilder.() -> Unit): KeyValueBucket
 
 	/**
-	 * Update the configuration of an existing bucket. The bucket name is required in the
-	 * builder block.
+	 * Update the configuration of an existing bucket.
+	 *
+	 * @param bucket name of the bucket to update
+	 * @param configure configuration to alter
 	 */
 	public suspend fun update(
 		bucket: String,
