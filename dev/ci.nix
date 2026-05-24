e2259f41f0d6f49b65f9450cbc50f9e20a379845
Nicholas Hassan <nick@hassan.host>
2026-05-24 22:24:03 +0930

    fix(ci): use jdk_headless to fix incompatability

--- dev/ci.nix
+++ dev/ci.nix
@@ -3,7 +3,7 @@
 {
   perSystem = { system, config, pkgs, lib, common, ... }: {
     devShells.ci = pkgs.mkMinimalShell {
-      nativeBuildInputs = common.packages;
+      nativeBuildInputs = map (p: if p == pkgs.jdk21 then pkgs.jdk21_headless else p) common.packages;
     };
   };
 }
